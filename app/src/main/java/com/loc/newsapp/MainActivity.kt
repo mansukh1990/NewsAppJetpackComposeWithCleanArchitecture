215e8b31129fb39b3ef4da472243de8b96b9b204
mansukh makwana <mansukh.m@celcius.in>
2024-12-19 15:57:19 +0530

    set page indicator - 19-12-24

--- app/src/main/java/com/loc/newsapp/MainActivity.kt
+++ app/src/main/java/com/loc/newsapp/MainActivity.kt
@@ -3,15 +3,27 @@ package com.loc.newsapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.ui.Modifier
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.core.view.WindowCompat
+import com.loc.newsapp.onboarding.OnBoardingScreen
 import com.loc.newsapp.ui.theme.NewsAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         installSplashScreen()
         setContent {
             NewsAppTheme {
+                Box(
+                    modifier = Modifier
+                ) {
+                    OnBoardingScreen()
+                }
 
             }
         }
